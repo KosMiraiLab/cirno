a358954e28fc2dd3cfcf453b547ce58bbae937fc
KosWu <ws00298046@163.com>
2023-10-14 17:19:18 +0800

    update

--- bot.py
+++ bot.py
@@ -1,17 +1,24 @@
 import nonebot
 from nonebot.adapters.mirai2 import Adapter as MIRAI2Adapter
+from nonebot.adapters.console import Adapter as ConcoleAdapter
+import typer
 
 
+def main(console_only: bool = False):
+    nonebot.init()
+    driver = nonebot.get_driver()
 
-nonebot.init()
+    adaptor = ConcoleAdapter if console_only else MIRAI2Adapter
+    if console_only:
+        driver.register_adapter(ConcoleAdapter)
+    else:
+        driver.register_adapter(MIRAI2Adapter)
 
-driver = nonebot.get_driver()
-driver.register_adapter(MIRAI2Adapter)
+    nonebot.load_builtin_plugins("echo")
+    nonebot.load_from_toml("pyproject.toml")
 
-nonebot.load_builtin_plugins('echo')
+    nonebot.run()
 
-
-nonebot.load_from_toml("pyproject.toml")
 
 if __name__ == "__main__":
-    nonebot.run()
+    typer.run(main)
